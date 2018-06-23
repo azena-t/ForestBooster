d1d581dc6725b590501499796095a14aa6eccc3c
Kevin Timmins <kevin.timmins@socralogic.co.uk>
2018-06-23 14:55:43 +0100

    Added the first level

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,6 +6,8 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
     AudioSource audioSource;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 1000f;
 
     // Use this for initialization
     void Start()
@@ -17,32 +19,53 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision other)
     {
-        HandleThrusting();
+        switch (other.gameObject.tag)
+        {
+            case "Friendly": 
+                print("OK");
+                
+                break;
+            default:
+                print("dead");
+                break;
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidbody.freezeRotation = false;
     }
 
-    private void HandleThrusting()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            float thrustThisFrame = mainThrust * Time.deltaTime;
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
 
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * thrustThisFrame);
         }
         else
         {
